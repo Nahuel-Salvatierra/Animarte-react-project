4aed2a1df844017ea0a831814e45c87c15fdf0fc
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-09-14 01:05:29 -0300

    Upgrade de Navbar
    Cambios necesarios del Navbar porque necesitamos tener 2 offcanvas. Uno para los Productos y otro para el Login y Sign Up

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,41 +1,72 @@
+import React, { useState } from "react";
 import InputBuscar from "./InputBuscar";
-import BotonIcono from "./BotonIcono";
+import IniciarSesion from "../pages/iniciarSesion/IniciarSesion";
 import '../estilos/navbar.css'
 
 
 function NavBar() {
+
+
+
+    const [productoActivo, setProductoActivo] = useState(null);
+
+    const activarProducto = (producto) => {
+        if (productoActivo === producto) {
+            // Si el mismo producto está activo, desactívalo
+            setProductoActivo(null);
+        } else {
+            setProductoActivo(producto);
+        }
+    };
+
+
     return (
-        <nav>
-            <nav className="navbar navbar-expand-lg fondo ">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src="https://http2.mlstatic.com/storage/mshops-appearance-api/images/86/160126086/logo-2021100113545352800.png" class="img-logo" alt="" />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto align-items-center text-center mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <BotonIcono
-                                    icono={'bi bi-instagram'}
-                                    link={'https://www.instagram.com/tienda.animarte/'} />
-                            </li>
-                            <li class="desplegable">
-                                <button class="dropbtn">PRODUCTOS</button>
-                                <div class="desplegable-content">
-                                    <a href="#">CUADERNOS</a>
-                                    <a href="#">REMERAS</a>
-                                </div>
-                            </li>
-                        </ul>
-                        <form className="d-flex" role="search">
-                            <InputBuscar />
-                        </form>
+        <nav className="navbar fondo">
+            <div className="container-fluid">
+                {/* Boton OffCanvas, cabiar boton a icono */}
+                <i className="bi bi-list fs-2 text-white" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" ></i>
+
+                {/* Logo Centrado */}
+                <a className="navbar-brand ms-5" href="#">
+                    <img src="https://http2.mlstatic.com/storage/mshops-appearance-api/images/86/160126086/logo-2021100113545352800.png" className="img-logo" alt="" />
+                </a>
+
+                {/* Contenido OffCanvas */}
+                <div
+                    className="offcanvas offcanvas-start"
+                    tabIndex="-1"
+                    id="offcanvasExample"
+                    aria-labelledby="offcanvasExampleLabel"
+                >
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+                            {/* Productos */}
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="offcanvas"
+                            aria-label="Close"
+                            onClick={() => setProductoActivo(null)}
+                        ></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        {/* Contenido Offcanvas */}
+                        Contenido de Productos, etc
+                        {/* Componente de lista de productos */}
                     </div>
                 </div>
-            </nav>
+                <div className="d-flex align-items-center">
+                    <div role="search" className="me-3">
+                        <InputBuscar />
+                    </div>
+                    <div>
+                        <IniciarSesion />
+                    </div>
+                </div>
+            </div>
         </nav>
+
     )
 }
 
